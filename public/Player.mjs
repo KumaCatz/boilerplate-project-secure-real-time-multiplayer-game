97c956ea7cb33b85f916399beec709e91f7ecf0b
KumaCatz <tod.kagan@gmail.com>
2024-12-27 11:42:08 -0300

    tests: completed

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,38 @@
 class Player {
   constructor({x, y, score, id}) {
-
+    this.x = x
+    this.y = y
+    this.score = score 
+    this.id = id
   }
 
   movePlayer(dir, speed) {
-
+    switch (dir) {
+      case 'left':
+        this.x -= speed
+        break
+      case 'right':
+        this.x += speed
+        break
+      case 'up':
+        this.y += speed
+        break
+      case 'down':
+        this.y -= speed
+        break
+    }
   }
 
   collision(item) {
-
+    if (this.x == item.x && this.y == item.y) {
+      return true
+    }
   }
 
   calculateRank(arr) {
-
+    const playersByScore = arr.sort((a, b) => b.score - a.score)
+    const playerPosition = playersByScore.findIndex(player => player.id == this.id) + 1
+    return `Rank: ${playerPosition}/${arr.length}`
   }
 }
 
